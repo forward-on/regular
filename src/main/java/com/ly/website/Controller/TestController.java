caa013b44bec6ecb4215b0ed84dacc6dc3e31314
liying <liying@ming800.com>
2018-03-08 16:01:13 +0800

    first config

---
+++ src/main/java/com/ly/website/Controller/TestController.java
@@ -0,0 +1,12 @@
+package com.ly.website.Controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by ly on 2018/3/8.
+ */
+@Controller
+public class TestController {
+
+
+}
